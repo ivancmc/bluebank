44073cca346c33112e927da8cdebd2884572b304
Ivan Cardoso <ivancmcosta@gmail.com>
2021-12-04 14:24:55 -0300

    Correção do formato de data.

--- Transacoes/src/main/java/com/squad/dezktop/service/AgendamentoService.java
+++ Transacoes/src/main/java/com/squad/dezktop/service/AgendamentoService.java
@@ -2,7 +2,7 @@ package com.squad.dezktop.service;
 
 import java.util.ArrayList;
 import java.sql.Date;
-import java.text.DateFormat;
+import java.text.*;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,11 +21,19 @@ public class AgendamentoService {
 
 	public List<TransacaoModel> efetuarAgendamento() throws Exception {
 
-		Date hoje = new Date(System.currentTimeMillis());
-		DateFormat f = DateFormat.getDateInstance(DateFormat.SHORT);
-		System.out.println(f.format(hoje).replace("-", "/"));
+		//Date hoje = new Date(System.currentTimeMillis());
+		//DateFormat f = DateFormat.getDateInstance(DateFormat.SHORT);
+		//System.out.println(f.format(hoje).replace("-", "/"));
+		
+		Date hoje = new Date();
+      		DateFormat df = new SimpleDateFormat("dd/MM/yyyy"); 
+      		Locale BRAZIL = new Locale("pt","BR");      
+      		df = DateFormat.getDateInstance(DateFormat.SHORT, BRAZIL);
+		
+		System.out.println(df.format(hoje));
 
-		List<AgendamentoModel> agendamentos = agendamentoRepository.getByAgendamento(f.format(hoje).replace("-", "/"));
+		//List<AgendamentoModel> agendamentos = agendamentoRepository.getByAgendamento(f.format(hoje).replace("-", "/"));
+		List<AgendamentoModel> agendamentos = agendamentoRepository.getByAgendamento(df.format(hoje));
 		List<TransacaoModel> transacoes = new ArrayList<>();
 
 		if (!agendamentos.isEmpty()) {
