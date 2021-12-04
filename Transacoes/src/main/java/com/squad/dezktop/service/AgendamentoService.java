ef74b525e795c095663b08c8cbe146b1c8990735
Ivan Cardoso <ivancmcosta@gmail.com>
2021-12-04 14:27:32 -0300

    Update AgendamentoService.java

--- Transacoes/src/main/java/com/squad/dezktop/service/AgendamentoService.java
+++ Transacoes/src/main/java/com/squad/dezktop/service/AgendamentoService.java
@@ -1,7 +1,7 @@
 package com.squad.dezktop.service;
 
 import java.util.ArrayList;
-import java.sql.Date;
+import java.util.Date;
 import java.text.*;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
