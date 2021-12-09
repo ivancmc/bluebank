0c8b5043986e51dc71cc167da31225ce8d3ae040
Ivan Cardoso <ivancmcosta@gmail.com>
2021-12-09 17:10:38 -0300

    Update DDL_database_BlueBank.sql

--- Arquivos/Script-SQL/DDL_database_BlueBank.sql
+++ Arquivos/Script-SQL/DDL_database_BlueBank.sql
@@ -1,12 +1,6 @@
-CREATE DATABASE `bluebank` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE `bluebank` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
-CREATE TABLE `agendamento` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `data` varchar(255) DEFAULT NULL,
-  `transacao_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK9wvvbhkdmevgg3m901fyuy7fg` (`transacao_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+-- bluebank.cliente definition
 
 CREATE TABLE `cliente` (
   `cpf` varchar(11) NOT NULL,
@@ -18,16 +12,10 @@ CREATE TABLE `cliente` (
   `senha` varchar(4) DEFAULT NULL,
   `telefone` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`cpf`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `conta` (
-  `numero_conta` varchar(10) NOT NULL,
-  `agencia` varchar(6) DEFAULT NULL,
-  `saldo` decimal(19,2) DEFAULT NULL,
-  `cliente_cpf` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`numero_conta`),
-  KEY `FK9gye6g3vk7w430iyrdynyogh` (`cliente_cpf`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- bluebank.conta_externa definition
 
 CREATE TABLE `conta_externa` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -39,7 +27,23 @@ CREATE TABLE `conta_externa` (
   `nome` varchar(255) DEFAULT NULL,
   `titulo` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- bluebank.conta definition
+
+CREATE TABLE `conta` (
+  `numero_conta` varchar(10) NOT NULL,
+  `agencia` varchar(6) DEFAULT NULL,
+  `saldo` decimal(19,2) DEFAULT NULL,
+  `cliente_cpf` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`numero_conta`),
+  KEY `FK9gye6g3vk7w430iyrdynyogh` (`cliente_cpf`),
+  CONSTRAINT `FK9gye6g3vk7w430iyrdynyogh` FOREIGN KEY (`cliente_cpf`) REFERENCES `cliente` (`cpf`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- bluebank.transacao definition
 
 CREATE TABLE `transacao` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -51,5 +55,18 @@ CREATE TABLE `transacao` (
   `valor` decimal(19,2) DEFAULT NULL,
   `conta_externa` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FKoh9siqwyxr594cc3vq6wjux0k` (`conta_externa`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+  KEY `FKoh9siqwyxr594cc3vq6wjux0k` (`conta_externa`),
+  CONSTRAINT `FKoh9siqwyxr594cc3vq6wjux0k` FOREIGN KEY (`conta_externa`) REFERENCES `conta_externa` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- bluebank.agendamento definition
+
+CREATE TABLE `agendamento` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `data` varchar(255) DEFAULT NULL,
+  `transacao_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK9wvvbhkdmevgg3m901fyuy7fg` (`transacao_id`),
+  CONSTRAINT `FK9wvvbhkdmevgg3m901fyuy7fg` FOREIGN KEY (`transacao_id`) REFERENCES `transacao` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
